9cc5cf619878d104d50da6022db68a8021dca79a
vijaykumar_exotel <vijay.kumar@exotel.in>
2023-01-12 23:16:18 +0530

    ami cred

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from src import loadgenerator
 from asterisk.ami import AMIClient
 
 client = AMIClient(address='localhost',port=5038)
-client.login(username='dacx',secret='debug')
+client.login(username='ameyodebug',secret='dacx')
 
 thread1 = loadgenerator.Thread("amithread",1 ,client)
 thread2 = loadgenerator.Thread("amithread",2 , client)
