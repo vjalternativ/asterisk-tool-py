79a0c4b25e0ad3a5ce9512ad9d376c978c47400e
vijaykumar_exotel <vijay.kumar@exotel.in>
2023-01-13 17:48:18 +0530

    changes

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from src import loadgenerator
 from src import amiserviceregistrar
 amiservice = amiserviceregistrar.AMIServiceRegistrar.registerami("callserver","localhost",5040,"ameyodebug","dacx")
 
-thread1 = loadgenerator.Thread("amithread",1 ,amiservice,5)
+thread1 = loadgenerator.Thread("amithread",1 ,amiservice,1)
 
 
 thread1.start()
