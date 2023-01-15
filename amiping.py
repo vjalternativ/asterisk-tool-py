fa1028a6918428247bb3c986b044dce7ce8d545f
vijaykumar_exotel <vijay.kumar@exotel.in>
2023-01-15 21:12:00 +0530

    changes

--- amiping.py
+++ amiping.py
@@ -11,12 +11,13 @@ def event_listener(event,**kwargs):
 client.add_event_listener(event_listener)
 client.login("ameyodebug", "dacx")
 
+def callback_response(response):
+    print(response)
 
 
 i =1
 while(i > 0):
     action = SimpleAction('Ping', ActionID="")
-    result = client.send_action(action)
-    print(result.response)
+    result = client.send_action(action,callback=callback_response)
     time.sleep(1)
     
