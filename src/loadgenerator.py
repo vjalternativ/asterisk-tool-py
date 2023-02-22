4e0444f72936d90e41d35d6e1a0304d5eb602043
vijaykumar_exotel <vijay.kumar@exotel.in>
2023-02-23 04:21:59 +0530

    changes

--- src/loadgenerator.py
+++ src/loadgenerator.py
@@ -102,7 +102,7 @@ class Thread(threading.Thread):
 
     def printreport(self):
         now  = datetime.today().strftime('%Y-%m-%d_%H:%M:%S')
-        path = f"loadreport-{self.maxcalls}-{now}"
+        path = f"loadreport-{self.maxcalls}-{self.start_time}-{now}"
         os.makedirs(path)
         ctx = f"{self.thread_name}_{self.thread_id}"
         
