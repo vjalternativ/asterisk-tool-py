85988a92751827efb5213a85723af04e4090fab4
vijaykumar_exotel <vijay.kumar@exotel.in>
2023-02-23 01:05:37 +0530

    changes

--- src/loadgenerator.py
+++ src/loadgenerator.py
@@ -1,6 +1,7 @@
 import threading
 import math
 import time
+import sys
 class Thread(threading.Thread):
     def __init__(self,thread_name,thread_id,amiservice,maxcalls, cps):
         threading.Thread.__init__(self)
@@ -10,16 +11,36 @@ class Thread(threading.Thread):
         self.maxcalls = maxcalls
         self.cps = cps
         self.channelsData =  {}
+        self.summary = {"total_channels" : 0, "dialstatus_vs_count" : {} , "hangup_cause_vs_count" : {}}
         amiservice.add_event_listener( 
             on_VarSet=self.on_VarSetEvent,
-            on_Newchannel=self.on_NewChannelEvent)
+            on_Newchannel=self.on_NewChannelEvent,
+            on_Hangup = self.on_Hangup,
+            on_DialEnd = self.on_DialEnd
 
+            )
+            
+    def on_DialEnd(self,event,**kwargs):
+        self.channelsData[event.keys['Channel']]['DialStatus'] = event.keys['DialStatus']
+        self.summary['hangup_cause_vs_count'][event.keys['DialStatus']] = self.summary['dialstatus_vs_count'][event.keys['DialStatus']] + 1
+
+
+    def on_Hangup(self, event, **kwargs):
+        self.channelsData[event.keys['Channel']]['hangupCause'] = event.keys['Cause']
+        self.channelsData[event.keys['Channel']]['hangupCauseText'] = event.keys['Cause-txt']
+        self.summary['hangup_cause_vs_count'][event.keys['Cause']] = self.summary['hangup_cause_vs_count'][event.keys['Cause']] + 1
+        
     def on_VarSetEvent(self, event,**kwargs):
-        self.channelsData[event.keys['Channel']][event.keys['Variable']] = event.keys['Value']
+        if event.keys['Variable']  in( "RTPAUDIOQOSRTT","RTPAUDIOQOSJITTER","RTPAUDIOQOSLOSS","SIPCALLID" ):
+            args = event.keys['Value'].split(',')
+            for arg in args:
+                keyval = arg.split('=')
+                self.channelsData[event.keys['Channel']][keyval[0]] = keyval[1]
     
     def on_NewChannelEvent(self, event, **kwargs):
         self.channelsData[event.keys['Channel']] = {}
-
+        self.summary['total_channels'] = self.summary['total_channels'] + 1
+ 
 
     def run(self) :
         ctx = f"{self.thread_name} : {self.thread_id}"
@@ -36,7 +57,9 @@ class Thread(threading.Thread):
         self.printreport()
 
     def printreport(self):
+        print(self.summary)
         print(self.channelsData)
+        sys.exit()
         
 
             
